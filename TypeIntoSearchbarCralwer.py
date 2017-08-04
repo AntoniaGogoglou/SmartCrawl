fd2b17b11b4badef6838eb33408fc5b2660916ae
Antonia Gogoglou <AntoniaGogoglou@users.noreply.github.com>
2017-08-04 06:39:13 +0300

    Update TypeIntoSearchbarCralwer.py

--- TypeIntoSearchbarCralwer.py
+++ TypeIntoSearchbarCralwer.py
@@ -12,25 +12,32 @@ urlsToCrawl=[]
 with open(filenameTitles,"rb+") as f:
     reader=csv.reader(f)
     for r in reader:
+        # this is to make a conference specific search
+        # a publication with the same title may exist in multiple venues so this way we select a particular venue
         urlsToCrawl.append([r[0]+' '+'SOFSEM'])
 f.close()
 
 for u in urlsToCrawl:
+    # path to the location of chromedriver 
     browser = webdriver.Chrome('/home/ust/chromedriver')
+    # load webpage
     browser.get('https://academic.microsoft.com/#/search')
+    # find search bar
     elem = browser.find_element_by_class_name("searchControl")
-    #elem = browser.find_element_by_name('p')  # Find the search box
+    # type into search bar and send
     elem.send_keys(u)
     elem.send_keys(Keys.ENTER)
     elem.send_keys(Keys.RETURN)
     curURL=browser.current_url
     print curURL
     if u is urlsToCrawl[0]:
+        # initiate file to write URLs
         with open(filenameURLs,"w+") as f:
             f.write("%s" % curURL)
             f.write("\n")
         f.close()
     else:        
+        # if the file already exists, append
         with open(filenameURLs,"a") as f:
             f.write("%s" % curURL)
             f.write("\n")
@@ -40,35 +47,4 @@ for u in urlsToCrawl:
 
 time.sleep(60)
 
-filenameTitles="URLsToBuild901_1015.txt"
-filenameURLs="urlsForMASFrompubTitlesMainProceedingsPart10.txt"
-urlsToCrawl=[]
-with open(filenameTitles,"rb+") as f:
-    reader=csv.reader(f)
-    for r in reader:
-        urlsToCrawl.append([r[0]+' '+'SOFSEM'])
-f.close()
 
-for u in urlsToCrawl:
-    browser = webdriver.Chrome('/home/anto/pyth_mysql_code/SOFSEM/chromedriver')
-    browser.get('https://academic.microsoft.com/#/search')
-    elem = browser.find_element_by_class_name("searchControl")
-    #elem = browser.find_element_by_name('p')  # Find the search box
-    elem.send_keys(u)
-    elem.send_keys(Keys.ENTER)
-    elem.send_keys(Keys.RETURN)
-    curURL=browser.current_url
-    print curURL
-    if u is urlsToCrawl[0]:
-        with open(filenameURLs,"w+") as f:
-            f.write("%s" % curURL)
-            f.write("\n")
-        f.close()
-    else:        
-        with open(filenameURLs,"a") as f:
-            f.write("%s" % curURL)
-            f.write("\n")
-        f.close()
-    time.sleep(5)
-    browser.quit()
-    
